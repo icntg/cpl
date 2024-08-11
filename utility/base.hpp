b7167f882717be42986fdb59ae2028db41b2a38c
starvii <starvii@gmail.com>
2024-08-12 01:11:59 +0800

    new Singleton

--- utility/base.hpp
+++ utility/base.hpp
@@ -2,12 +2,16 @@
 #define BASE_7O35IAG3GPSZD6XSQPLM7KG2_H
 
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <string>
-#include <memory>
+#include <windows.h>
 
 //单例模板
 //delete 与 default 来自于 c++ 11
 // 有一个缺陷，无法在其中使用智能指针…
+// 智能指针依赖AcquireSRWLockExclusive与ReleaseSRWLockExclusive这两个系统函数，
+// 而XP中没有这两个函数。如果需要兼容WindowsXP的话，需要避免使用智能指针。
 /*
 使用方式：
 class Logger final : public Singleton<Logger> {
@@ -18,25 +22,68 @@ public:
     ~Logger() override {...}
 };
  */
+using namespace std;
+
 template <typename T>
 class Singleton
 {
+    T* _instance = nullptr;
+    PCRITICAL_SECTION _pcs = nullptr;
 public:
-    static T& GetInstance()
+    static T& Instance(const PCRITICAL_SECTION pCriticalSection = nullptr)
     {
-        static T instance;
-        return instance;
+        // C++11实现方式自带锁，会导致不兼容XP
+        static T* instance = nullptr;
+        if (nullptr == instance)
+        {
+            if (nullptr != pCriticalSection)
+            {
+                EnterCriticalSection(pCriticalSection);
+
+            }
+            if (nullptr == instance)
+            {
+                instance = new T();
+                instance->_instance = instance; // 记录一下，释放时使用。
+                instance->_pcs = pCriticalSection; // 记录一下，释放时使用。
+            }
+            if (nullptr != pCriticalSection)
+            {
+                LeaveCriticalSection(pCriticalSection);
+            }
+        }
+        return *instance;
+    }
+
+    /**
+     * 实际上该方法不需要调用。
+     * 由系统回收资源即可。
+     */
+    static void Destory()
+    {
+        auto& t = Instance();
+        auto pcs = t._pcs;
+        if (nullptr != pcs)
+        {
+            EnterCriticalSection(pcs);
+        }
+        if (nullptr != t._instance)
+        {
+            delete t._instance;
+        }
+        if (nullptr != pcs)
+        {
+            LeaveCriticalSection(pcs);
+        }
+        pcs = nullptr;
     }
 
     explicit Singleton(T&&) = delete;
-
     explicit Singleton(const T&) = delete;
-
-    void operator=(const T&) = delete;
+    Singleton& operator=(const T&) = delete;
 
 protected:
     Singleton() = default;
-
     virtual ~Singleton() = default;
 };
 
@@ -44,8 +91,8 @@ protected:
 class ISerialize
 {
 public:
-    virtual std::string serialize() =0;
-    virtual int32_t deserialize(const std::string& s) =0;
+    virtual std::string Serialize() =0;
+    virtual int32_t Deserialize(const std::string& s) =0;
     virtual ~ISerialize() = default;
 };
 
@@ -60,8 +107,8 @@ class ISerializeJson : public ISerialize
 class IContext
 {
 public:
-    virtual int32_t load() =0;
-    virtual int32_t unload() =0;
+    virtual int32_t Load() =0;
+    virtual int32_t Unload() =0;
     virtual ~IContext() = default;
 };
 
